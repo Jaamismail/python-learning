57abe837567f268dea3b590fa5115bbabc81b2d7
Jaamismail <jaamismail85@gmail.com>
2026-03-15 09:06:03 +0000

    Today i learn syntax, output in output i learn print text

---
+++ pythonday1/syntax.py
@@ -0,0 +1,17 @@
+if 5 > 2:
+  print("Five is greater than two!")
+
+print("Python is fun!")
+
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")
+
+print("Hello"); print("How are you?"); print("Bye bye!")
+
+print("This will work!")
+print('This will also work!')
+
+
+print("Hello World!", end=" ")
+print("I will print on the same line.")
